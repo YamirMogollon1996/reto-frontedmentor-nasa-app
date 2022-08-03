18f1348ee265fbd9e5ccf731d5fd704157d9887b
yamir mogollon <mogollonosorioyamir@gmail.com>
2022-08-03 08:55:45 -0500

    se agrego proyecto nasa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Page1 from './Componentes/page1';
+import  { BrowserRouter , Route, Routes }   from "react-router-dom"
+import Paginarais from './Componentes/Paginarais';
+import Prew from './Componentes/crew';
+import Tecnology from './Componentes/Tecnology';
+import Rutas from './Componentes/Rutas';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+  
+        <div className='header' >
+  
+          <h1 className='top_brand'>
+          </h1>
+          <div  className='raya-top'  >     </div>
+         
+          <Rutas></Rutas>
+
+        </div>
+                
+
+{/* 
+              <Rutas></Rutas>         */}
+
+
+                <Routes>
+        
+                    <Route  path='/'  element={ <Paginarais></Paginarais>  } ></Route>
+                    <Route   path='/destination'  element={  <Page1></Page1> }  ></Route> 
+                    <Route path='/crew' element= {  <Prew></Prew>  }  ></Route>
+                    <Route  path='/technoly'   element={  <Tecnology></Tecnology>  } ></Route>
+
+                </Routes>
+        
+        
+        </BrowserRouter>
   );
 }
 
